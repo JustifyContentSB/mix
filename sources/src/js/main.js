b7853522ca91ac15ec48c2a25dd247b6eea133cc
RxPro <wxweb@mail.ru>
2022-01-10 12:01:22 +0300

    Верстка главной страницы с адаптивом. Жду работы дизайнера над попапами, меню и следующими страницами

--- sources/src/js/main.js
+++ sources/src/js/main.js
@@ -1,9 +1,34 @@
 "use strict";
 
-$(document).ready(function(){
+$(document).ready(function () {
     // Your code here
-    
+
+    if ($(window).width() < 1024) {
+        bestTracksSwiper()
+        bestPlaylistSwiper()
+        bestArtistSwiper()
+    }
 });
 
 // Your functions here
+function bestTracksSwiper() {
+    var swiper = new Swiper('.best__tracks-swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 12,
+    });
+}
+
+function bestPlaylistSwiper() {
+    var swiper = new Swiper('.best__playlist-swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 12,
+    });
+}
+
+function bestArtistSwiper() {
+    var swiper = new Swiper('.best__artist-swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 12,
+    });
+}
 
